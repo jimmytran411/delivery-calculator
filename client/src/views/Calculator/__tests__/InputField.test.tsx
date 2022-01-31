7d3e3b9949188800cca63902f7b8b3d233876ab2
An tran <jimmytran411@gmail.com>
2022-01-31 12:28:27 +0200

    update input field test

--- client/src/views/Calculator/__tests__/InputField.test.tsx
+++ client/src/views/Calculator/__tests__/InputField.test.tsx
@@ -3,18 +3,14 @@ import userEvent from '@testing-library/user-event';
 import { InputField } from '../Components/InputField';
 
 test('It should display inputfield component, user should be able to type into the input field and it should display error if there is any', () => {
-  const handleInput = jest.fn();
-  const { getByText, getByLabelText, rerender } = render(
-    <InputField label="test field" name="test field" error="" handleInput={handleInput} />
-  );
+  const { getByText, getByLabelText, rerender } = render(<InputField label="test field" name="test field" error="" />);
 
   const field = getByLabelText(/test field/i);
   userEvent.type(field, '12.5');
 
-  expect(handleInput).toHaveBeenCalled();
   expect(field).toHaveValue('12.5');
 
-  rerender(<InputField label="test field" name="test field" error="test error" handleInput={handleInput} />);
+  rerender(<InputField label="test field" name="test field" error="test error" />);
   const error = getByText(/test error/i);
-  expect(error).toBeInTheDocument();
+  expect(error).toHaveTextContent('test error');
 });
