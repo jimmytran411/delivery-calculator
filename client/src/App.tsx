8b80389d6dc10f30678b9920f610f7e651d4b8c1
An tran <jimmytran411@gmail.com>
2022-02-17 14:37:34 +0200

    clean up code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
 import { makeStyles } from '@material-ui/core';
 
-import makeStore from './rootRedux/store';
-import { history } from './rootRedux/store';
 import { Calculator } from './views/Calculator';
 import { CalendarProvider } from './views/Calculator/context/CalendarContext';
-
-export const store = makeStore();
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
 const useAppStyles = makeStyles(() => ({
   root: {
@@ -22,15 +14,11 @@ const useAppStyles = makeStyles(() => ({
 const App: React.FC = () => {
   const { root } = useAppStyles();
   return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <div className={root}>
-          <CalendarProvider>
-            <Calculator />
-          </CalendarProvider>
-        </div>
-      </ConnectedRouter>
-    </Provider>
+    <div className={root}>
+      <CalendarProvider>
+        <Calculator />
+      </CalendarProvider>
+    </div>
   );
 };
 
--- client/src/rootRedux/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { connectRouter } from "connected-react-router";
-
-const rootReducer = (history: any) => {
-  return combineReducers({ router: connectRouter(history) });
-};
-
-export type Store = ReturnType<typeof rootReducer>;
-export default rootReducer;
